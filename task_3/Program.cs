3858f9e4d5385cb09af16d8f3525a5a51d0ed1ad
Lyas <Egorlyasotsky@yandex.ru>
2023-04-03 22:38:31 +0400

    task number 3 is ready

--- task_3/Program.cs
+++ task_3/Program.cs
@@ -4,3 +4,17 @@
 
 6, 1, 33 -> [6, 1, 33] */
 
+int[] Array(int elem)
+{
+  int[] mass = new int[elem];
+  for(int i = 0; i < elem; i++)
+  {
+    mass[i] = new Random().Next(0, 100);
+  }
+return mass;
+  }
+
+  Console.Clear();
+  int[] array = Array(8);
+  Console.WriteLine($"[{String.Join(",", array)}]");
+
