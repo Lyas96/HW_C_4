9252813b575a5e5f93d8db76f0b1eebcc2f1ac0a
Lyas <Egorlyasotsky@yandex.ru>
2023-04-03 21:44:31 +0400

    task number 2 is ready

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -6,3 +6,19 @@
 
 9012 -> 12 */
 
+int NumSum(int n)
+{
+  int sum = 0;
+  int a = n;
+  for(int i = 0; n > 0; i++)
+  {
+    a = n % 10;
+    sum += a;
+    n /= 10;
+  }
+  return sum;
+}
+
+Console.Write("Enter number N: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine($"Sum number {n} = {NumSum(n)}");
